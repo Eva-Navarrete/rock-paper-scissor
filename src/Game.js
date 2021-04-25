308035c3e03c4f6cec4f8eb758bbfa38253893fe
Eva Navarrete <evanavarrete07@gmail.com>
2021-04-24 19:22:31 -0600

    Add some if statements

--- src/Game.js
+++ src/Game.js
@@ -13,13 +13,6 @@ class Game {
     // add all five tokens in here first put the 3 easy tokens then the last two that make it difficult if a game this.type = diffcicult use all 5 if its classic use first 3
   }
 
-
-  // chooseGame() {
-  //   // which game type is chosen 'classic' or 'difficult'
-    // might NOT NEED THIS METHOD IF I HAVE thhis.type property.
-  //
-  // }
-  //
   chooseRandomIndex() {
 
     var randomIndex = Math.floor(Math.random() * this.fighter.length);
@@ -33,53 +26,30 @@ class Game {
       this.fighter = ['calcifer', 'spritz', 'haku'];
     }
   }
-  //pass in the token choice's for computer for both classic and difficult game. this.frigher = ['haku',]
-  //
-  // fight(event) {
-  //   this.humanWeapon = (event.target.id)
-  //
-  //   // var fighters = this.fighter;
-  //   // var humanWeapon = this.humanWeapon;
-  //   //
-  //   // humanWeapon = fighter(event.target.id);
-  //
-  //   // if (event.target.id === 'calcifer') {
-  //   //   this.humanWeapon
-  //   // } else if (event.target.id === 'haku') {
-  //   //   this.humanWeapon
-  //   // } else if (event.target.id === 'spritz') {
-  //   //   this.humanWeapon
-  //   // }
-  //
-  // }
 
-  // make a method which checks gametype if its difficult you get all five if its classic you get 3
+  playClassicGame(chooseRandomIndex) {
+    if (this.human.choice === 'haku' && this.computer.choice === 'calcifer') {
+      this.human.isWinner = true;
+    } else if (this.human.choice === 'spritz' && this.computer.choice === 'haku') {
+      this.human.isWinner = true;
+    } else if (this.human.choice === 'calcifer' && this.computer.choice === 'spritz') {
+      this.human.isWinner = true;
+    } else if (this.computer.choice === 'haku' && this.human.choice === 'calcifer'){
+      this.computer.isWinner = true;
+    }else if (this.computer.choice === 'spritz' && this.human.choice === 'haku'){
+      this.computer.isWinner =true;
+    }else if (this.computer.choice === 'calcifer' && this.human.choice === 'spritz') {
+      this.computer.isWinner = true;
+    } else if (this.human.choice === this.computer.choice){
+      this.isWinner = null;
+    }
 
-
-  playClassicGame() {
-      if (this.human.choice === 'haku' && this.computer.choice === 'calcifer') {
-        this.human.isWinner = true;
-        console.log('1st',this.human);
-      } else if (this.human.chocie === 'spritz' && this.computer.choice === 'haku') {
-        this.human.isWinner = true;
-        console.log('2nd',this.human);
-      } else if (this.humanWeapon === 'calcifer' && this.computerWeapon === 'spritz') {
-        this.human.isWinner = true;
-        console.log('3rd',this.human);
-      }
-
-    // /calciferFighter
-    // pass in the players instance since they're choosing a game token.
-    // which game token is being chosen.
-  //   function results (choice) {
-
-  //
-  //
-  //   // will need conditionals of which game token beats the other
   }
 
-  displayDifficultGame() {
 
+
+  playDifficultGame() {
+    console.log("hello World");
   }
 
   checkWinner() {
